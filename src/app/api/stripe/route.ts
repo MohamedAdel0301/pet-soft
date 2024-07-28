a3e8bc7a71415970de3e9bd8ba5f1ec94de62488
MohamedAdel0301 <mosoliman33@gmail.com>
2024-07-28 07:47:23 +0300

    added verification webhook

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,16 +1,40 @@
 import prisma from "@/lib/db";
-import { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const data = await request.json();
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+export async function POST(request: Request) {
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature");
+
+  // verify webhook came from Stripe
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (error) {
+    console.log("Webhook verification failed", error);
+    return Response.json(null, { status: 400 });
+  }
+
+  // fulfill order
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // return 200 OK
   return Response.json(null, { status: 200 });
 }
